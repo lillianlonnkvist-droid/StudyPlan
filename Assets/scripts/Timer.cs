92909c6b54d0441b7b90684d2471d387534fb818
vadymbilik-lgtm <vadym.bilik@elev.ga.lbs.se>
2026-03-03 14:01:46 +0100

    timer

--- Assets/scripts/Timer.cs
+++ Assets/scripts/Timer.cs
@@ -1,16 +1,61 @@
-using UnityEngine;
+﻿using UnityEngine;
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public TMP_Text timeText;
 
-    // Update is called once per frame
+    private float timeSeconds = 0f;
+    private bool isRunning = false;
+
     void Update()
     {
-        
+        if (!isRunning) return;
+
+        if (timeSeconds > 0)
+        {
+            timeSeconds -= Time.deltaTime;
+            UpdateText();
+        }
+        else
+        {
+            timeSeconds = 0;
+            isRunning = false;
+            UpdateText();
+            Debug.Log("Time finished!");
+        }
+    }
+
+    
+    public void Add10Minutes()  // + 10 min
+    {
+        timeSeconds += 600f;
+        UpdateText();
+    }
+
+   
+    public void Minus10Minutes()  // -10 min
+    {
+        timeSeconds -= 600f;
+
+        if (timeSeconds < 0)
+            timeSeconds = 0;
+
+        UpdateText();
+    }
+
+    // 🟢 Start
+    public void StartTimer()
+    {
+        if (timeSeconds > 0)
+            isRunning = true;
+    }
+
+    void UpdateText()
+    {
+        int minutes = Mathf.FloorToInt(timeSeconds / 60);
+        int seconds = Mathf.FloorToInt(timeSeconds % 60);
+
+        timeText.text = $"{minutes:00}:{seconds:00}";
     }
 }
