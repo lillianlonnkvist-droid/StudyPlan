0edf8ec5b162e9a0faa3707333d2b3f000600698
vadymbilik-lgtm <vadym.bilik@elev.ga.lbs.se>
2026-03-05 14:26:33 +0100

    Files11

---
+++ Assets/scripts/Scenechanger.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadSceneApp(string StudieApp)
+    {
+        SceneManager.LoadScene(StudieApp);
+    }
+    public void LoadSceneSettings(string Settings)
+    {
+        SceneManager.LoadScene(Settings);
+    }
+  
+}
